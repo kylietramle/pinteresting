d1be2cd5d20a700b18a340bdff231916aef0669a
Kylie Le <kylie.tram.le@gmail.com>
2015-07-22 14:11:45 -0700

    Added jQuery Masonry

---
+++ db/migrate/20150721010643_add_user_id_to_profile.rb
@@ -0,0 +1,4 @@
+class AddUserIdToProfile < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720200518) do
+ActiveRecord::Schema.define(version: 20150721010643) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
